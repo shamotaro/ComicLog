2a8bf54ebe844dcc9a5d25ab2ef325ed4aa501c5
shamotaro <75371156+shamotaro@users.noreply.github.com>
2021-06-14 00:33:52 +0900

    認証フロー追加

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+//このコンポーネントでは、withAuthenticator コンポーネントを使用
+// このコンポーネントは、ユーザー認証フロー全体を足場にして、
+// ユーザーがサインアップ、サインイン、パスワードのリセット、
+// および多要素認証 (MFA) のサインインの確認を行えるようにします。
+// サインアウトボタンをレンダリングする AmplifySignOut コンポーネントも使用
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Amplify プロジェクトで使用できるさまざまな AWS リソース
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
